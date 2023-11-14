edbb084d269322fbf4c743162c4fd1f626350840
hannabakker <150758658+hannabakker@users.noreply.github.com>
2023-11-14 14:43:21 +1100

    Update MyV0Component.tsx

--- app/components/MyV0Component.tsx
+++ app/components/MyV0Component.tsx
@@ -1,62 +1,32 @@
-// you can overwrite this entire file with your v0 Component.
-// just copy and paste the "React" output over the entire file.
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/iLoXMB9u0v1
+ */
+import { Button } from "@/components/ui/button"
 
-import Link from "next/link";
-import React from "react";
-import {V0Logo} from "./symbols";
-
-function TestComponent() {
+export default function Component() {
   return (
-    <div
-      style={{
-        display: "flex",
-        height: "100%",
-        justifyContent: "space-between",
-        flexDirection: "column",
-        width: "100%",
-      }}
-    >
-      <h3
-        style={{
-          maxWidth: "11em",
-        }}
-      >
-        This would be a pretty good place for a{" "}
-        <Link href="https://v0.dev/" target="_blank" rel="noopener noreferrer">
-          v0 component
-        </Link>
-        , wouldn't it?
-      </h3>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "flex-end",
-          width: "100%",
-        }}
-      >
-        <p
+    <div className="relative h-screen flex items-center justify-center bg-red-500">
+      <div className="absolute inset-0 bg-red-600 opacity-50" />
+      <div className="relative z-10 text-center p-6 bg-white bg-opacity-80 rounded-md shadow-lg">
+        <img
+          alt="Love Heart"
+          className="mx-auto mb-4 filter hue-rotate-120"
+          height="100"
+          src="/placeholder.svg"
           style={{
-            maxWidth: "20em",
+            aspectRatio: "100/100",
+            objectFit: "cover",
           }}
-        >
-          Go make one, then paste it into{" "}
-          <code
-            style={{
-              fontFamily: "var(--font-geist-mono)",
-              fontWeight: 500,
-              fontSize: "0.95em",
-              fontFeatureSettings: "'ss09'",
-            }}
-          >
-            app/components/MyV0Component.tsx
-          </code>
-        </p>
-
-        <V0Logo />
+          width="100"
+        />
+        <blockquote className="mb-4 text-2xl font-semibold">
+          "Stay far from timid. Only make moves when your heart's in it, and live the phrase 'Sky's The Limit.'"
+        </blockquote>
+        <cite className="mb-6 block text-lg text-right">- Biggie Smalls</cite>
+        <Button variant="outline">New Quote</Button>
       </div>
     </div>
-  );
+  )
 }
 
-export default TestComponent;
